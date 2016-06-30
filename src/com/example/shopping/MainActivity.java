5b9411c72b084368058a2008bcb5f41193e453da
YJY <2651167692@qq.com>
2016-06-30 20:32:50 +0800

    制作了跳转界面

--- src/com/example/shopping/MainActivity.java
+++ src/com/example/shopping/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.shopping;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,14 +12,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(MainActivity.this,ShoppingActivity.class);
+				startActivity(i);
+				MainActivity.this.finish();
+			}
+		}, 2000);
     }
+    
+    
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    
     
 }
---
+++ src/com/example/shopping/ShoppingActivity.java
@@ -0,0 +1,13 @@
+package com.example.shopping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoppingActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceBundle){
+		super.onCreate(savedInstanceBundle);
+		setContentView(R.layout.shopping_main);
+	}
+
+}
